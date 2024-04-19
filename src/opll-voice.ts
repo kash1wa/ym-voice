38533e2bc258fd756208309a0ab0eec4227c0c6b
kash1wa <148605002+kash1wa@users.noreply.github.com>
2024-04-19 21:02:01 +0900

    Update opll-voice.ts

--- src/opll-voice.ts
+++ src/opll-voice.ts
@@ -215,7 +215,7 @@ export class OPLLVoice extends YMVoice {
       var v = new OPNVoice({
         fb: s[0].ws ? Math.min(7, this.fb + 6) : this.fb,
         con: 2,
-        ams: 4,
+        ams: 2,
 	      pms: s[0].pm || s[1].pm ? 2 : 0,
 	      slots: [
 	        s[0].toOPN(false),
@@ -229,14 +229,14 @@ export class OPLLVoice extends YMVoice {
 	      ]
 	    });
 	    v.slots[1].ml = s[0].ml;
-	    v.slots[2].ml = s[0].ml <= 7 ? s[0].ml * 2 : 15;
+	    v.slots[2].ml = Math.min(15, s[0].ml * 2);
 	    if (v.slots[1].ml == 0) {
 			  v.slots[1].ml = 1;
 		    v.slots[2].ml = 2;
 			}
-	    voi.slots[0].tl = (voi.slots[0].tl <= 96) ? voi.slots[0].tl + 15 : 96;
-	    voi.slots[1].tl = (voi.slots[1].tl <= 96) ? voi.slots[1].tl + 15 : 96;
-	    voi.slots[2].tl = (voi.slots[2].tl <= 96) ? voi.slots[2].tl + 22 : 96;
+	    voi.slots[0].tl = Math.min(127, voi.slots[0].tl + 15);
+	    voi.slots[1].tl = Math.min(127, voi.slots[0].tl + 15);
+	    voi.slots[2].tl = Math.min(127, voi.slots[2].tl + 22);
 	    return v;
     } else
     if (s[0].ws === 0 && s[1].ws === 1) {
@@ -244,7 +244,7 @@ export class OPLLVoice extends YMVoice {
 	    return new opn_voice_1.OPNVoice({
 	      fb: s[0].ws ? Math.min(7, this.fb + 6) : this.fb,
 	      con: 2,
-	      ams: 4,
+	      ams: 2,
 	      pms: s[0].pm || s[1].pm ? 2 : 0,
 	      slots: [
 	        s[0].toOPN(false),
@@ -267,7 +267,7 @@ export class OPLLVoice extends YMVoice {
       return new OPNVoice({
         fb: s[0].ws ? Math.min(7, this.fb + 6) : this.fb,
         con: 2,
-        ams: 4, // 5.9dB
+        ams: 2, // 5.9dB
         pms: s[0].pm || s[1].pm ? 2 : 0, // 6.7cent or 0
         slots: [
           s[0].toOPN(false),
